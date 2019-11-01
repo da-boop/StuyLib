c1281362d8d3d447e8c50b7b7278abaa19b6bf2b
Sam Belliveau <sam.belliveau@gmail.com>
2019-11-01 16:26:19 -0400

    prepare to redo NetKeyWindow

--- src/com/stuypulse/stuylib/input/keyboard/computer/NetKeyWindow.java
+++ src/com/stuypulse/stuylib/input/keyboard/computer/NetKeyWindow.java
@@ -40,44 +40,15 @@ public class NetKeyWindow extends JFrame {
         // Set Title and Open Network Table
         super("Network Keyboard Input");
 
-        // Get team number from user
-        int team;
-        do {
-            try {
-                String teamNum = JOptionPane.showInputDialog("Enter Team Number:");
-                team = Integer.parseInt(teamNum);
-            } catch(Exception e) {
-                team = -1;
-            }
-        } while(team < 0);
+        // TODO: Get team number from user
+        int team = 694;
 
-        // Get keyboard port from user
-        int port = 0;
-        try {
-            String keyboardPort = JOptionPane.showInputDialog("Enter Virtual Keyboard Port (Default=0):");
-            port = Integer.parseInt(keyboardPort);
-        } catch(Exception e) { 
-            port = 0;
-        }
+        // TODO: Get keyboard port from user
+        int port = 1735;
 
-        // Connect NetKeyListener
+        // TODO: Connect NetKeyListener
         mListener = new NetKeyListener(team, port);
         addKeyListener(mListener);
-
-        // Set Title
-        setTitle("Network Keyboard Input [Team: " + team + ", Port: " + port + "]");
-
-        // Message
-        JLabel message = new JLabel("Sending Keyboard Input to [Team: " + team + ", Port: " + port + "]");
-        message.setBorder(new LineBorder(Color.BLACK, 4));
-
-        // Message Panel
-        final JPanel messagePanel = new JPanel(new GridBagLayout());
-        messagePanel.add(message);
-        getContentPane().add(messagePanel);
-
-        // Pack and Set Size
-        pack();
 
         // Set Visible
         setVisible(true);
