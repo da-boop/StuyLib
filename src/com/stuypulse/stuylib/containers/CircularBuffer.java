c63e3bd903f9cfaf8e686f133b44035fa565b9ca
Sam Belliveau <sam.belliveau@gmail.com>
2019-11-26 19:47:53 -0500

    formatted document

--- src/com/stuypulse/stuylib/containers/CircularBuffer.java
+++ src/com/stuypulse/stuylib/containers/CircularBuffer.java
@@ -3,73 +3,83 @@ package com.stuypulse.stuylib.containers;
 import java.util.AbstractCollection;
 import java.util.Iterator;
 
-public class CircularBuffer<T> extends AbstractCollection<T>{
-    public class CircularBufferIterator<T> implements Iterator<T>{
+/**
+ * 
+ * @author Kevin (pretty cool dude for making this)
+ * @param <T> type
+ */
+public class CircularBuffer<T> extends AbstractCollection<T> {
+    
+    public class CircularBufferIterator<T> implements Iterator<T> {
         private CircularBuffer<T> ref;
         int start;
         int end;
-        public CircularBufferIterator(CircularBuffer<T> buf){
+
+        public CircularBufferIterator(CircularBuffer<T> buf) {
             ref = buf;
             start = ref.head;
-            end = ref.head+ref.mSize;
+            end = ref.head + ref.mSize;
         }
-        public boolean hasNext(){
-            return (ref.buffer[(start+1) % ref.mSize] != null) && start < end;
+
+        public boolean hasNext() {
+            return (ref.buffer[(start + 1) % ref.mSize] != null) && start < end;
         }
-        public T next(){
-            if(hasNext()){
+
+        public T next() {
+            if (hasNext()) {
                 return ref.buffer[start++ % ref.mSize];
             }
             return null;
         }
-        public T[] toArray(){
+
+        public T[] toArray() {
             T[] out = (T[]) new Object[ref.mSize];
-            for(int i =0;hasNext();i++){
+            for (int i = 0; hasNext(); i++) {
                 out[i] = next();
             }
             return out;
         }
     }
-    
+
     public static final int kDefaultSize = 50;
 
     private final int mSize;
     private int head;
     private T[] buffer;
 
-    /* need to be finished*/
-    public CircularBuffer(int size){
+    /* need to be finished */
+    public CircularBuffer(int size) {
         mSize = size;
         buffer = (T[]) new Object[size];
     }
 
-    public T getHead(){
+    public T getHead() {
         return buffer[head];
     }
 
-    public T get(int index){
-        return buffer[(head+(int)Math.signum(index) *(Math.abs(index)%mSize))%mSize];
+    public T get(int index) {
+        return buffer[(head + (int) Math.signum(index) * (Math.abs(index) % mSize)) % mSize];
     }
 
-    public T getTail(){
-        return buffer[(head-1)%mSize];
+    public T getTail() {
+        return buffer[(head - 1) % mSize];
     }
 
-    public boolean add(T in){
-        buffer[head++%mSize] = in;
+    public boolean add(T in) {
+        buffer[head++ % mSize] = in;
         return true;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return buffer[head] == null;
     }
-    public int size(){
+
+    public int size() {
         return mSize;
     }
 
-    public Iterator<T> iterator(){
+    public Iterator<T> iterator() {
         return new CircularBufferIterator<T>(this);
     }
-
 
 }
